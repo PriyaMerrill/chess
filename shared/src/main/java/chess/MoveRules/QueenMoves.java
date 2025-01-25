17040f374b8e81850b70356ae2c45482526a8cc6
PriyaMerrill <priyamerrill@gmail.com>
2025-01-24 21:40:27 -0700

    Successfully passed queen

---
+++ shared/src/main/java/chess/MoveRules/QueenMoves.java
@@ -0,0 +1,14 @@
+package chess.MoveRules;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.List;
+
+public class QueenMoves {
+    public static List<ChessMove> makeMoves(ChessBoard board, ChessPosition startPosition){
+        int [][] directions = {{-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}};
+        return MoveCalculator.possibleMoves(board, startPosition, directions);
+    }
+}
