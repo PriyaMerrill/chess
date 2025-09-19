965c44298817ce96cb1e37f746e0ee557c8eeb7f
PriyaMerrill <priyamerrill@gmail.com>
2025-09-19 13:05:54 -0600

    making the function that is going to come up with all the moves

--- shared/src/main/java/MoveRules/MoveCalculator.java
+++ shared/src/main/java/MoveRules/MoveCalculator.java
@@ -1,12 +1,23 @@
 package MoveRules;
 
-import chess.ChessPosition;
+import chess.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoveCalculator {
     static boolean onBoard(ChessPosition position){
         int row = position.getRow();
         int col = position.getColumn();
         return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+    }
+    static List<ChessMove> generateMoves(ChessBoard board, ChessPosition startPosition, int[][] directions, boolean isLimited) {
+        List<ChessMove> moves = new ArrayList<>();
+        ChessPiece startPiece = board.getPiece(startPosition);
+        ChessGame.TeamColor team = startPiece.getTeamColor();
+
+
+        return moves;
     }
 
 }
