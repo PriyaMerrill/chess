2f22b8be2fb61299ca6254eb11cd397111f34240
PriyaMerrill <priyamerrill@gmail.com>
2025-12-06 23:30:33 -0700

    Stalemate works!

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -170,7 +170,10 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        return false;
+        if (isInCheck(teamColor)) {
+            return false;
+        }
+        return cantMove(teamColor);
     }
 
 
