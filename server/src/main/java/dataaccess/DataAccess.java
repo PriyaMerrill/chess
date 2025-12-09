b157e094025a25836bcb3be10bfca6c5fd3011dd
PriyaMerrill <priyamerrill@gmail.com>
2025-12-08 21:35:33 -0700

    throw all exceptions

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -4,7 +4,19 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import javax.xml.crypto.Data;
+import java.util.Collection;
 
 
 public interface DataAccess {
+    void clear() throws DataAccessException;
+    void createUser(UserData username) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    void auth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String auth) throws DataAccessException;
+    void authRemove(String auth) throws DataAccessException;
+    int newGame(GameData game) throws DataAccessException;
+    GameData getGame(int game) throws DataAccessException;
+    Collection<GameData> games() throws DataAccessException;
+    void gameUpdate(GameData game) throws DataAccessException;
 }
