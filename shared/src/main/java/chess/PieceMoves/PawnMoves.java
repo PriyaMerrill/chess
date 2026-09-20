b397389ac2033b2b302f73de260789afda00d121
Priya Merrill <priyamerrill@gmail.com>
2026-09-19 19:51:59 -0600

    Pawn promotion works, all pawn tests passed.

--- shared/src/main/java/chess/PieceMoves/PawnMoves.java
+++ shared/src/main/java/chess/PieceMoves/PawnMoves.java
@@ -12,13 +12,16 @@ public class PawnMoves implements MovesCalculator{
         ChessGame.TeamColor myTeam = board.getPiece(position).getTeamColor();
         int moveForward;
         int startRow;
+        int endRow;
 
         if(myTeam == ChessGame.TeamColor.WHITE) {
             moveForward = 1;
             startRow = 2;
+            endRow = 8;
         } else {
             moveForward = -1;
             startRow = 7;
+            endRow = 1;
         }
 
         int row = position.getRow();
@@ -28,22 +31,22 @@ public class PawnMoves implements MovesCalculator{
         ChessPosition newSpot = new ChessPosition(nextRow, col);
 
         if ((nextRow >= 1 && nextRow <= 8) && (board.getPiece(newSpot) == null )){
-            movePawn.add(new ChessMove(position, newSpot, null));
+            addPawnMove(movePawn, position, newSpot, endRow);
 
             //a pawn can move two spots on the first turn
             //row of the square two ahead positive for white negative for black
             int twoStep = row+moveForward*2;
             ChessPosition moveTwo = new ChessPosition(twoStep, col);
             if ((row == startRow) && (board.getPiece(moveTwo) == null)){
-                movePawn.add(new ChessMove(position, moveTwo, null));
+                addPawnMove(movePawn, position, moveTwo, endRow);
             }
         }
-        pawnCapture(board, position, movePawn, myTeam, nextRow);
+        pawnCapture(board, position, movePawn, myTeam, nextRow, endRow);
 
         return movePawn;
     }
 
-    private void pawnCapture(ChessBoard board, ChessPosition position, Collection<ChessMove>movePawn, ChessGame.TeamColor myTeam, int nextRow){
+    private void pawnCapture(ChessBoard board, ChessPosition position, Collection<ChessMove>movePawn, ChessGame.TeamColor myTeam, int nextRow, int promotionRow){
         int col = position.getColumn();
         int[] diagonals = {-1,1};
         for (int diagonal : diagonals){
@@ -53,9 +56,20 @@ public class PawnMoves implements MovesCalculator{
                 ChessPiece targetPiece = board.getPiece(captureSpot);
 
                 if ((targetPiece != null) && (targetPiece.getTeamColor() != myTeam)){
-                    movePawn.add(new ChessMove(position, captureSpot, null));
+                    addPawnMove(movePawn, position, captureSpot, promotionRow);
                 }
             }
+        }
+    }
+
+    private void addPawnMove(Collection<ChessMove> movePawn, ChessPosition start, ChessPosition end, int promotionRow){
+        if (end.getRow() == promotionRow){
+            movePawn.add(new ChessMove(start,end, ChessPiece.PieceType.QUEEN));
+            movePawn.add(new ChessMove(start,end, ChessPiece.PieceType.BISHOP));
+            movePawn.add(new ChessMove(start,end, ChessPiece.PieceType.KNIGHT));
+            movePawn.add(new ChessMove(start,end, ChessPiece.PieceType.ROOK));
+        } else {
+            movePawn.add(new ChessMove(start, end, null));
         }
     }
 }
